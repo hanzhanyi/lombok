3da9833e9f148c1fecffa5151b2ebdaaa4986bcd
hanzhanyi <hanzhanyi@qiyi.com>
2018-08-21 22:13:29 +0800

    lombok

--- src/main/java/com/iqiyi/lombok/annotations/DataExample.java
+++ src/main/java/com/iqiyi/lombok/annotations/DataExample.java
@@ -1,8 +1,14 @@
 package com.iqiyi.lombok.annotations;
 
+import lombok.NonNull;
+
 /**
  * Created by hanzhanyi on 2018/8/20.
  */
 @lombok.Data
 public class DataExample {
+    private String a;
+
+    @NonNull
+    private String b;
 }
